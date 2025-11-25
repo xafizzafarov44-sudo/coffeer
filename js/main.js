8e1bb1d980363c8fd7b9eacb27939ed0ab2babcf
xafizzafarov44-sudo <xafizzafarov44@gmail.com>
2025-11-25 19:08:50 +0500

    createt

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+    const burger = document.querySelector(".burger");
+    const menu = document.querySelector(".header__list");
+
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("active");
+        menu.classList.toggle("active");
+    });
